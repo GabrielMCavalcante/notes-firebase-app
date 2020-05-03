c8e2eee7a25b07fd7d9fbbd8e11b4ea0462e2699
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-05-03 14:12:56 -0300

    added EditNote component

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,27 +1,37 @@
 <template>
     <div class="home container">
         <Navigation :navOptions="navOptions"></Navigation>
-        <component :is="'Overview'" @overview="changeOptions"></component>
+        <component 
+            :is="component" 
+            @changeOptions="changeOptions" 
+            @changeView="changeView"
+        ></component>
     </div>
 </template>
 
 <script>
 import Navigation from './navigation/Navigation.vue'
 import Overview from './pages/Overview.vue'
+import EditNote from './pages/EditNote.vue'
 export default {
     name: 'Home',
     components: {
         Navigation,
-        'Overview': Overview
+        'Overview': Overview,
+        'EditNote': EditNote
     },
     data() {
         return {
-            navOptions: new Array()
+            navOptions: new Array(),
+            component: 'Overview'
         }
     },
     methods: {
         changeOptions(options) {
-            this.navOptions = options;
+            this.navOptions = options
+        },
+        changeView(view) {
+            this.component = view
         }
     }
 }
