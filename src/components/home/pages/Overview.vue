c0db9972145418307026daea136b3e9e25d76fd4
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-05-03 14:14:29 -0300

    added multiselection logic

--- src/components/home/pages/Overview.vue
+++ src/components/home/pages/Overview.vue
@@ -5,7 +5,15 @@
       <!-- Note cards -->
       <v-col cols="3" v-for="note in filteredNotes" :key="note.id">
         <v-hover v-slot:default="{ hover }">
-          <v-card :elevation="hover?'8':'2'" class="grey lighten-3 note">
+          <v-card @click="clickHandler(note)" :elevation="hover?'8':'2'" class="grey lighten-3 note">
+            <v-checkbox
+              v-if="multiselect"
+              v-model="note.selected"
+              @click="note.selected = !note.selected"
+              :color="note.color.toLowerCase()"
+              hide-details
+              class="check-note"
+            ></v-checkbox>
             <div class="filter-bar" :style="{background: note.color}"></div>
             <v-card-title>{{note.title | minifyTitle}}</v-card-title>
             <v-card-text>{{note.body | minifyContent}}</v-card-text>
@@ -37,20 +45,27 @@ export default {
   name: "Overview",
   data() {
     return {
+      multiselect: false,
       options: [
-        { text: "Add note", icon: "mdi-plus", type: "normal" },
+        { 
+          text: "Add note", 
+          icon: "mdi-plus", 
+          type: "normal", 
+          click: function(){console.log('add note')} 
+        },
         {
           text: "Order By",
           slug: "order-by",
           first: "Title",
           icon: "mdi-order-alphabetical-ascending",
           type: "dropdown",
-          options: ["Title", "Creation", "Modification"]
+          options: ["Title", "Creation", "Modification"],
+          click: function(){}
         },
         {
           text: "Filter",
           slug: "filter",
-          first: "All",
+          first: 'All',
           icon: "mdi-flag",
           type: "dropdown",
           options: [
@@ -64,14 +79,20 @@ export default {
             "Blue",
             "Black",
             "White"
-          ]
+          ],
+          click: function(){}
         },
-        {
-          text: "Multiselection",
+        { text: "Multiselection",
           icon: "mdi-vector-arrange-above",
-          type: "normal"
+          type: "normal",
+          click: ()=>{this.multiselection()}
         },
-        { text: "Deleted notes", icon: "mdi-trash-can-outline", type: "normal" }
+        { 
+          text: "Deleted notes", 
+          icon: "mdi-trash-can-outline", 
+          type: "normal",
+          click: function(){console.log('deleted notes')} 
+        }
       ]
     };
   },
@@ -91,11 +112,20 @@ export default {
     }
   },
   methods: {
-    ...mapActions(["setNotes"])
+    ...mapActions(["setNotes", "setView"]),
+    clickHandler(note) {
+      if(!this.multiselect)
+        this.$emit('changeView', 'EditNote')
+      else note.selected = !note.selected
+    },
+    multiselection() {
+      this.multiselect = !this.multiselect
+    }
   },
   created() {
-    this.$emit("overview", this.options);
-    this.setNotes();
+    this.$emit("changeOptions", this.options)
+    this.setView('Overview')
+    this.setNotes()
   },
   filters: {
     minifyTitle(value) {
@@ -127,5 +157,11 @@ export default {
 .overview .filter-bar {
   width: 100%;
   height: 5px;
+}
+.overview .check-note {
+  margin: 0 0 0 90%;
+  width: fit-content;
+  height: fit-content;
+  padding: 0;
 }
 </style>
