7e44857b688d591e41843358c5f2cb056e9697ad
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-05-04 15:36:32 -0300

    initialized firebase app connection

---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBTvczkPKF5Ftp23C9sDKYrLx7U3KMlWcY",
+    authDomain: "notes-5cf6a.firebaseapp.com",
+    databaseURL: "https://notes-5cf6a.firebaseio.com",
+    projectId: "notes-5cf6a",
+    storageBucket: "notes-5cf6a.appspot.com",
+    messagingSenderId: "1014809961003",
+    appId: "1:1014809961003:web:8bce82a789a68337d6006e"
+}
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+export default firebaseApp.firestore()
