42790dd7db6d478f6d0563a8ef983e80890cf131
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-05-05 17:13:35 -0300

    added multiselection logic and options

--- src/components/home/pages/Overview.vue
+++ src/components/home/pages/Overview.vue
@@ -1,51 +1,88 @@
 <template>
-  <v-container class="overview d-inline-flex">
-    <!-- Loading circle -->
-    <div v-if="!loaded">
-      <v-progress-circular
-        class="loading"
-        indeterminate
-        rotate
-        size="120"
-        color="grey darken-2"
-      ></v-progress-circular>
+  <div class="overview">
+    <div v-if="notesSelected.length >= 1">
+      <div class="buttons">
+        <div class="d-inline-flex">
+          <v-btn @click="selectAll" class="multiselection-option">
+            <v-icon left>mdi-select-all</v-icon>
+            Select all
+          </v-btn>
+          <v-btn @click="unselectAll" class="multiselection-option">
+            <v-icon left>mdi-select-off</v-icon>
+            Unselect all
+          </v-btn>
+          <v-btn @click="invertAll" class="multiselection-option">
+            <v-icon left>mdi-select-inverse</v-icon>
+            Invert selection
+          </v-btn>
+          <v-btn @click="deleteSelected" class="red lighten-1 white--text">
+            <v-icon left>mdi-delete-circle-outline</v-icon>
+            Delete
+          </v-btn>
+        </div>
+      </div>
     </div>
-    <v-row v-else>
-      <!-- Note cards -->
-      <v-col cols="3" v-for="note in filteredNotes" :key="note.id">
-        <v-hover v-slot:default="{ hover }">
-          <v-card @click="clickHandler(note)" :elevation="hover?'8':'2'" class="grey lighten-3 note">
-            <v-checkbox
-              v-if="multiselect"
-              v-model="note.selected"
-              @click="note.selected = !note.selected"
-              :color="note.color.toLowerCase()"
-              hide-details
-              class="check-note"
-            ></v-checkbox>
-            <div class="filter-bar" :style="{background: note.color}"></div>
-            <v-card-title>{{note.title | minifyTitle}}</v-card-title>
-            <v-card-text>{{note.body | minifyContent}}</v-card-text>
-          </v-card>
-        </v-hover>
-      </v-col>
 
-      <!-- Add new note card -->
-      <v-col cols="3" v-if="loaded">
-        <v-hover v-slot:default="{ hover }">
-          <v-card @click="addNewNote" outlined class="pa-3 note add-note" :elevation="hover?'8':'2'">
-            <v-card-title>
-              <span class="mx-auto">Add Note</span>
-            </v-card-title>
-            <v-card-text class="add-sign">
-              <v-icon size="100">mdi-plus</v-icon>
-            </v-card-text>
-          </v-card>
-        </v-hover>
-      </v-col>
+    <v-container class="d-inline-flex">
+      <!-- Loading circle -->
+      <div v-if="!loaded">
+        <v-progress-circular
+          class="loading"
+          indeterminate
+          rotate
+          size="120"
+          color="grey darken-2"
+        ></v-progress-circular>
+      </div>
 
-    </v-row>
-  </v-container>
+      <v-row v-else>
+        <!-- Note cards -->
+        <v-col cols="12" sm="4" md="3" v-for="note in filteredNotes" :key="note.id">
+          <v-hover v-slot:default="{ hover }">
+            <v-card 
+              @click="clickHandler(note)" 
+              :elevation="hover?'8':'2'" 
+              class="grey lighten-3 note"
+            >
+              <v-checkbox
+                v-if="multiselect"
+                :value="hovering"
+                v-model="note.selected"
+                @click="note.selected = !note.selected"
+                :color="note.color.toLowerCase()"
+                hide-details
+                class="check-note"
+              ></v-checkbox>
+
+              <div class="filter-bar" :style="{background: note.color}"></div>
+              <v-card-title>{{note.title | minifyTitle}}</v-card-title>
+              <v-card-text>{{note.body | minifyContent}}</v-card-text>
+            </v-card>
+          </v-hover>
+        </v-col>
+
+        <!-- Add new note card -->
+        <v-col cols="12" sm="4" md="3" v-if="loaded">
+          <v-hover v-slot:default="{ hover }">
+            <v-card 
+              @click="addNewNote" 
+              outlined 
+              class="pa-3 note add-note" 
+              :elevation="hover?'8':'2'"
+            >
+              <v-card-title>
+                <span class="mx-auto">Add Note</span>
+              </v-card-title>
+              <v-card-text class="add-sign">
+                <v-icon size="100">mdi-plus</v-icon>
+              </v-card-text>
+            </v-card>
+          </v-hover>
+        </v-col>
+
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -55,6 +92,8 @@ export default {
   data() {
     return {
       multiselect: false,
+      hovering: false,
+      notesSelected: new Array(),
       options: [
         { 
           text: "Add note", 
@@ -106,7 +145,7 @@ export default {
     };
   },
   computed: {
-    ...mapGetters(["loaded", "notes", "search", "order", "filter", "currentNote"]),
+    ...mapGetters(["loaded", "notes", "search", "order", "filter", "currentNote", "trash"]),
     filteredNotes() {
       return this.notes.filter(note => {
         if (note.title.match(this.search)) {
@@ -121,13 +160,20 @@ export default {
     }
   },
   methods: {
-    ...mapActions(["setNotes", "setView", "setCurrentNote", "addNote"]),
+    ...mapActions(["setNotes", "setView", "setCurrentNote", "addNote", "sendToTrash"]),
     clickHandler(note) {
       if(!this.multiselect) {
         this.setCurrentNote(note)
         this.$emit('changeView', 'EditNote')
       }
-      else note.selected = !note.selected // multiselection logic here!!
+      else {
+        note.selected = !note.selected
+        if(note.selected) {
+          this.notesSelected.push(note)
+        } else {
+          this.notesSelected = this.notesSelected.filter(n=>n.id!==note.id)
+        }
+      } 
     },
     addNewNote() {
       const newNote = {
@@ -144,6 +190,38 @@ export default {
     },
     multiselection() {
       this.multiselect = !this.multiselect
+      if(!this.multiselect) {
+        const resetNotesSelectedState = this.notes.filter(note=>{
+          if(note.selected)
+            note.selected = false
+          return note
+        })
+        this.$store.commit('setNotes', resetNotesSelectedState)
+        this.setNotes(resetNotesSelectedState)
+        this.notesSelected = new Array()
+      }
+    },
+    selectAll() {
+      this.notes.forEach(note=>note.selected = true)
+      this.notesSelected = this.notes
+    },
+    unselectAll() {
+      this.notes.forEach(note=>note.selected = false)
+      this.notesSelected = new Array()
+    },
+    invertAll() {
+      this.notesSelected = new Array()
+      this.notes.forEach(note=>{
+        note.selected = note.selected ? false : true
+        if(note.selected) this.notesSelected.push(note)
+      })
+    },
+    deleteSelected() {
+      const response = confirm('All notes selected will be sent to the trash. Continue?')
+      if(response) {
+        this.sendToTrash(this.notesSelected)
+          .catch(err=>alert(`Error while deleting note: ${err.message}`))
+      }
     },
     capitalize(string) {
       let result = string.split('')
@@ -172,6 +250,13 @@ export default {
 </script>
 
 <style>
+.overview .buttons {
+  width: fit-content;
+  margin: 0 auto;
+}
+.overview .multiselection-option {
+  margin-right: 10px;
+}
 .overview .loading {
   position: absolute;
   top: 50%;
