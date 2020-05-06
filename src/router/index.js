c1e1a20f5f3ee784fe2bba3988dba3b2dcf72d27
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-05-06 15:56:13 -0300

    added route security logic

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Register from '@/components/userRegister/Register.vue'
 import Home from '@/components/home/Home.vue'
+import firebase from 'firebase'
 
 Vue.use(VueRouter)
 
@@ -14,14 +15,29 @@ const routes = [
   {
     path: '/home',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
   }
 ]
 
 const router = new VueRouter({
+  strict: true,
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next)=>{
+  if(to.matched.some(rec=>rec.meta.requiresAuth)) {
+      const user = firebase.auth().currentUser;
+      if(user) next()
+      else {
+        alert('You must be logged in')
+        next({name: 'Register'})
+      }
+  } else next()
 })
 
 export default router
