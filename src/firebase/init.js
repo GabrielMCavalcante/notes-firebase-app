f8209240c170c7bf2183489f7af48c45410e0f38
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-15 10:11:52 -0300

    Deleted unused file

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "your apiKey",
-    authDomain: "your authDomain",
-    databaseURL: "your databaseURL",
-    projectId: "your projectId",
-    storageBucket: "your storageBucket",
-    messagingSenderId: "your messagingSenderId",
-    appId: "your appId"
+    apiKey: "AIzaSyBTvczkPKF5Ftp23C9sDKYrLx7U3KMlWcY",
+    authDomain: "notes-5cf6a.firebaseapp.com",
+    databaseURL: "https://notes-5cf6a.firebaseio.com",
+    projectId: "notes-5cf6a",
+    storageBucket: "notes-5cf6a.appspot.com",
+    messagingSenderId: "1014809961003",
+    appId: "1:1014809961003:web:8bce82a789a68337d6006e"
 }
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig)
