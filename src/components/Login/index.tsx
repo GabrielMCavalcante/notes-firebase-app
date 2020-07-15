8277c6fb9edbe4a8700fb9d1b3093e1e11c59a94
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-15 18:51:37 -0300

    Refactored to use Form component

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,31 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-// Icons
-import { MdEmail, MdLock } from 'react-icons/md'
+// Components
+import Form from 'components/UI/Form'
 
 // CSS styles
 import './styles.css'
 
+// Icons
+import { Icon } from '@iconify/react'
+import lock from '@iconify/icons-mdi/lock'
+import email from '@iconify/icons-mdi/email'
+
+const fields = [{
+    label: '',
+    name: 'email',
+    type: 'email',
+    config: { placeholder: 'example@domain.com' },
+    validation: /([a-z\d.-]+)@([a-z\d-]+)(\.[a-z]{2,8}(\.[a-z]{2,8})?)/i,
+    feedback: 'The email has to be in a valid email format ex.: example@domain.com',
+    isValid: false,
+    touched: false,
+    value: '',
+    icon: <Icon icon={email} />
+},
+{
+    label: '',
+    name: 'password',
+    type: 'password',
+    config: { placeholder: 'abcd1234' },
+    validation: /^[a-z0-9]{8,16}$/i,
+    feedback: 'The password has to contain 8-16 characters.',
+    isValid: false,
+    touched: false,
+    value: '',
+    icon: <Icon icon={lock} />
+}]
+
 function Login() {
+
+    const [loading, setLoading] = useState(false)
+
+    function onLogin(fieldsValues: { [key: string]: string }) {
+        setLoading(true)
+        console.log(fieldsValues)
+    }
+
     return (
         <div className="Login">
             <h2>Login</h2>
-            <form>
-                <div className="Field">
-                    <MdEmail />
-                    <input name="email" type="text" />
-                    <label htmlFor="email" className="label-email">
-                        <span className="label-content">Your Email</span>
-                    </label>
-                </div>
-                <div className="Field">
-                    <MdLock />
-                    <input name="password" type="password" />
-                    <label htmlFor="password" className="label-password">
-                        <span className="label-content">Your Password</span>
-                    </label>
-                </div>
-            </form>
+            <Form
+                fields={fields}
+                onsubmit={fieldsValues => onLogin(fieldsValues)}
+                btnLabel="Login"
+                loading={loading}
+                resetForm
+            />
         </div>
     )
 }
