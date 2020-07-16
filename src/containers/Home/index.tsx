f8420d3633b12a3552a1134e459645d6701f2fe3
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-16 15:55:23 -0300

    Created Home container

---
+++ src/containers/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// Components
+import Navigation from 'containers/Navigation'
+
+function Home() {
+    return (
+        <div className="Home">
+            <Navigation />
+
+        </div>
+    )
+}
+
+export default Home
