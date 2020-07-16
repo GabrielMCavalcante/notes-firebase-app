d815ca6696c3cf176d5e6d779c122f95ef869dec
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-16 15:55:03 -0300

    Added Home route

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,12 +3,14 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 
 // Containers
 import Register from './Register'
+import Home from './Home' 
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/" exact component={Register}/>
+        <Route path="/home" component={Home}/>
         <Redirect to="/"/>
       </Switch>
     </div>
