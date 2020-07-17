775269f8f905338fc49dbe7215dca12c912e7364
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-17 11:33:39 -0300

    Added Overview route

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { Switch, Route, Redirect } from 'react-router'
 
 // Components
 import Navigation from 'containers/Navigation'
+
+// Containers
+import Overview from 'containers/Overview'
 
 function Home() {
     return (
         <div className="Home">
             <Navigation />
-
+            <Switch>
+                <Route exact path="/home/overview" component={Overview}/>
+                <Redirect to="/home/overview" />
+            </Switch>
         </div>
     )
 }
