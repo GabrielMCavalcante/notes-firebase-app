123e9e4b84fcdf1d97ad1dea32829aa0287d060c
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-20 13:49:25 -0300

    Setted up redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,41 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './containers/App'
 
+// Redux
+import {
+    createStore,
+    applyMiddleware,
+    combineReducers
+} from 'redux'
+
+// Redux Provider
+import { Provider } from 'react-redux'
+
+// Redux Thunk
+import thunk from 'redux-thunk'
+
+// Store reducers
+import overviewReducer from 'store/reducers/overview'
+import navigationReducer from 'store/reducers/navigation'
+
+// Redux Devtools
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+// Combining reducers into one
+const rootReducer = combineReducers({
+    overview: overviewReducer,
+    navigation: navigationReducer
+})
+
+const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(thunk)
+))
+
 const app = (
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>
 )
 
