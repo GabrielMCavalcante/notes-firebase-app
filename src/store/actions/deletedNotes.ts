9ec39f2c31247c368d9c213345c14e586c450ee6
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-21 11:19:15 -0300

    Created initial DeletedNotes action creators

---
+++ src/store/actions/deletedNotes.ts
@@ -0,0 +1,16 @@
+// Action Types
+import deletedNotesTypes from 'store/actions/actionTypes/deletedNotesTypes'
+
+// Redux Dispatch type
+import { Dispatch } from 'redux'
+
+// Interfaces
+import { Note } from 'interfaces'
+
+const actions = {
+    setTrash: (trash: Note) => ({ type: deletedNotesTypes.SET_TRASH, trash })
+}
+
+export default {
+    setTrash: (trash: Note) => (dispatch: Dispatch) => dispatch(actions.setTrash(trash))
+}
