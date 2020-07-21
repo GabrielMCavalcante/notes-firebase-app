4b63e2cc79b41a47913cf0873b2204899738c063
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-21 11:18:49 -0300

    Imported custom interfaces

--- src/store/actions/navigation.ts
+++ src/store/actions/navigation.ts
@@ -4,13 +4,17 @@ import navTypes from 'store/actions/actionTypes/navigationTypes'
 // Redux Dispatch type
 import { Dispatch } from 'redux'
 
+// Interfaces
+import { Note, Option } from 'interfaces'
+
 const actions = {
-    setOptions: (options: any) => ({type: navTypes.SET_OPTIONS, options}),
-    toggleMultiselection: () => ({ type: navTypes.TOGGLE_MULTISELECTION })
+    setOptions: (options: Option[]) => ({type: navTypes.SET_OPTIONS, options}),
+    toggleMultiselection: () => ({ type: navTypes.TOGGLE_MULTISELECTION }),
+    setCurrentNote: (currentNote: Note) => ({ type: navTypes.SET_CURRENT_NOTE, currentNote})
 }
 
 export default {
-    setOptions(options: any) { 
+    setOptions(options: Option[]) { 
         return function(dispatch: Dispatch) {
             dispatch(actions.setOptions(options))
         }
@@ -18,6 +22,11 @@ export default {
     toggleMultiselection() {
         return function(dispatch: Dispatch) { 
             dispatch(actions.toggleMultiselection())
+        }
+    },
+    setCurrentNote(currentNote: Note) {
+        return function(dispatch: Dispatch) {
+            dispatch(actions.setCurrentNote(currentNote))
         }
     }
 }
--- src/store/reducers/navigation.ts
+++ src/store/reducers/navigation.ts
@@ -3,25 +3,40 @@ import { AnyAction } from 'redux'
 // Action Types
 import navTypes from 'store/actions/actionTypes/navigationTypes'
 
+// Interfaces
+import { Note, Option } from 'interfaces'
+
+interface State {
+    options: Option[],
+    multiselection: boolean,
+    currentNote: Note | null
+}
+
+// Initial State
 const initialState = {
     options: [],
     multiselection: false,
-    mode: 'overview'
+    currentNote: null
 }
 
-export default function NavigationReducer(state = initialState, action: AnyAction) {
+export default function NavigationReducer(state: State = initialState, action: AnyAction) {
     
-    function toggleMultiselection(state: any) {
+    function toggleMultiselection(state: State) {
         return { ...state, multiselection: !state.multiselection }
     }
 
-    function setOptions(state: any, options: any) {
+    function setOptions(state: State, options: Option[]) {
         return { ...state, options }
+    }
+
+    function setCurrentNote(state: State, currentNote: Note) {
+        return { ...state, currentNote }
     }
 
     switch(action.type) {
         case navTypes.SET_OPTIONS: return setOptions(state, action.options)
         case navTypes.TOGGLE_MULTISELECTION: return toggleMultiselection(state)
+        case navTypes.SET_CURRENT_NOTE: return setCurrentNote(state, action.currentNote)
         default: return state
     }
 }
