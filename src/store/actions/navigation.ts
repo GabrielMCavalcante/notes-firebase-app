a056220f9aca87f9b23544eaa82072bd8babd68b
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-22 17:25:47 -0300

    Added navigation action creators

--- src/store/actions/navigation.ts
+++ src/store/actions/navigation.ts
@@ -8,36 +8,36 @@ import { Dispatch } from 'redux'
 import { Note, Option } from 'interfaces'
 
 const actions = {
-    setOptions: (options: Option[]) => ({type: navTypes.SET_OPTIONS, options}),
+    setOptions: (options: Option[]) => ({ type: navTypes.SET_OPTIONS, options }),
     toggleMultiselection: () => ({ type: navTypes.TOGGLE_MULTISELECTION }),
-    setCurrentNote: (currentNote: Note) => ({ type: navTypes.SET_CURRENT_NOTE, currentNote}),
-    setCurrentUser: (currentUser: any) => ({ type: navTypes.SET_CURRENT_USER, currentUser}),
-    setSearch: (search: string) => ({ type: navTypes.SET_SEARCH , search})
+    setCurrentNote: (currentNote: Note) => ({ type: navTypes.SET_CURRENT_NOTE, currentNote }),
+    setCurrentUser: (currentUser: any) => ({ type: navTypes.SET_CURRENT_USER, currentUser }),
+    setSearch: (search: string) => ({ type: navTypes.SET_SEARCH, search })
 }
 
 export default {
-    setOptions(options: Option[]) { 
-        return function(dispatch: Dispatch) {
+    setOptions(options: Option[]) {
+        return function (dispatch: Dispatch) {
             dispatch(actions.setOptions(options))
         }
     },
     toggleMultiselection() {
-        return function(dispatch: Dispatch) { 
+        return function (dispatch: Dispatch) {
             dispatch(actions.toggleMultiselection())
         }
     },
     setCurrentNote(currentNote: Note) {
-        return function(dispatch: Dispatch) {
+        return function (dispatch: Dispatch) {
             dispatch(actions.setCurrentNote(currentNote))
         }
     },
     setCurrentUser(currentUser: any) {
-        return function(dispatch: Dispatch) {
+        return function (dispatch: Dispatch) {
             dispatch(actions.setCurrentUser(currentUser))
         }
     },
     setSearch(search: string) {
-        return function(dispatch: Dispatch) {
+        return function (dispatch: Dispatch) {
             dispatch(actions.setSearch(search))
         }
     }
