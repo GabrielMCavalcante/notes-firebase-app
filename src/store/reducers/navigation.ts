32df2b57da861e1ed8c7673a73ef4d8a6d1d6f47
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-21 21:15:09 -0300

    Added setSearch reducer function

--- src/store/reducers/navigation.ts
+++ src/store/reducers/navigation.ts
@@ -10,7 +10,8 @@ interface State {
     options: Option[],
     multiselection: boolean,
     currentNote: Note | null,
-    currentUser: any | null
+    currentUser: any | null,
+    search: string | null
 }
 
 // Initial State
@@ -18,7 +19,8 @@ const initialState = {
     options: [],
     multiselection: false,
     currentNote: null,
-    currentUser: null
+    currentUser: null,
+    search: null
 }
 
 export default function NavigationReducer(state: State = initialState, action: AnyAction) {
@@ -39,11 +41,16 @@ export default function NavigationReducer(state: State = initialState, action: A
         return { ...state, currentUser }
     }
 
+    function setSearch(state: State, search: string) {
+        return { ...state, search }
+    }
+
     switch(action.type) {
         case navTypes.SET_OPTIONS: return setOptions(state, action.options)
         case navTypes.TOGGLE_MULTISELECTION: return toggleMultiselection(state)
         case navTypes.SET_CURRENT_NOTE: return setCurrentNote(state, action.currentNote)
         case navTypes.SET_CURRENT_USER: return setCurrentUser(state, action.currentUser)
+        case navTypes.SET_SEARCH: return setSearch(state, action.search)
         default: return state
     }
 }
