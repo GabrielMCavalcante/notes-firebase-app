90050c9798be3fb788988a29b170f1a6c73ff33b
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-23 12:51:16 -0300

    Added new global functions

--- src/globalfn.ts
+++ src/globalfn.ts
@@ -4,29 +4,63 @@ import { database, auth } from 'firebase/init'
 // Interfaces
 import { Note } from 'interfaces'
 
-export function sendToTrash(notesToTrash: Note[]) {
-    return new Promise(resolve => {
+export function sendNotesTo(from: "notes" | "trash", to: "notes" | "trash", notesToSend: Note[]) {
+    return new Promise<{notes: Note[], trash: Note[]}>(resolve => {
         const user = auth.currentUser?.uid
         database.collection('users').doc(user).get()
             .then(snapshot => {
-                const notes: Note[] = snapshot.data()!.notes
-                const trash: Note[] = snapshot.data()!.trash
+                const notesContainers: {notes: Note[], trash: Note[], [key: string]: any} = {
+                    notes: snapshot.data()!.notes,
+                    trash: snapshot.data()!.trash
+                }
+                
+                const origin: Note[] = notesContainers[from]
+                const destiny: Note[] = notesContainers[to]
 
-                notesToTrash.forEach(noteToTrash => {
-                    notes.forEach((note, index) => {
-                        if (noteToTrash.id === note.id) {
-                            notes.splice(index, 1)
+                notesToSend.forEach(noteToSend => {
+                    origin.forEach((note, index) => {
+                        if (noteToSend.id === note.id) {
+                            origin.splice(index, 1)
                             Object.assign(
-                                noteToTrash,
-                                { deleted: Date.now(), selected: false }
+                                noteToSend,
+                                { 
+                                    [to==='trash'?'deleted':'created']: Date.now(), 
+                                    selected: false 
+                                }
                             )
-                            trash.push(noteToTrash)
+                            destiny.push(noteToSend)
                         }
                     })
                 })
+                
+                const notes = from === 'notes' ? origin : destiny
+                const trash = to === 'trash' ? destiny : origin
 
                 database.collection('users').doc(user).update({ notes, trash })
                     .then(() => resolve({ notes, trash }))
             })
+    })
+}
+
+export function sortFunction(a: any, b: any, order: string) {
+    const elA = typeof a[order] === 'string' ? a[order].toLowerCase() : a[order]
+    const elB = typeof b[order] === 'string' ? b[order].toLowerCase() : b[order]
+    if (elA > elB) return 1
+    else if (elA < elB) return -1
+    else return 0
+}
+
+export function selectAll(filteredNotes: Note[]) {
+    return filteredNotes.map(note => ({ ...note, selected: true }))
+}
+
+export function unselectAll(filteredNotes: Note[]) {
+    return filteredNotes.map(note => ({ ...note, selected: false }))
+}
+
+export function invertSelection(filteredNotes: Note[]) {
+    return filteredNotes.map(note => {
+        if (note.selected) return { ...note, selected: false }
+        else return { ...note, selected: true }
     })
 }
