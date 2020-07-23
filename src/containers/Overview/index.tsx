5bb3414030e655b21fa203b1b72c0a36a3ace25a
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-23 12:57:50 -0300

    Changed multiselection behaviour

--- src/containers/Overview/index.tsx
+++ src/containers/Overview/index.tsx
@@ -23,17 +23,26 @@ import { Icon } from '@iconify/react'
 import selectAllIcon from '@iconify/icons-mdi/select-all'
 import selectOffIcon from '@iconify/icons-mdi/select-off'
 import selectInverseIcon from '@iconify/icons-mdi/select-inverse'
-import deleteCircleOutlineIcon from '@iconify/icons-mdi/delete-circle-outline'
+import deleteCircleIcon from '@iconify/icons-mdi/delete-circle'
 import plusIcon from '@iconify/icons-mdi/plus'
 import plus from '@iconify/icons-mdi/plus'
 import vector_arrange_above from '@iconify/icons-mdi/vector-arrange-above'
-import trash_can_outline from '@iconify/icons-mdi/trash-can-outline'
+import trashCanIcon from '@iconify/icons-mdi/trash-can'
 
 // CSS styles
 import './styles.css'
 
 // Interfaces
 import { Note, Option } from 'interfaces'
+
+// Global functions 
+import {
+    selectAll,
+    unselectAll,
+    sendNotesTo,
+    sortFunction,
+    invertSelection
+} from 'globalfn'
 
 interface Props {
     notes: Note[],
@@ -48,18 +57,11 @@ function Overview(props: Props) {
     const [order, setOrder] = useState('title')
     const [loading, setLoading] = useState(true)
 
-    function sortFunction(a: any, b: any) {
-        const elA = typeof a[order] === 'string' ? a[order].toLowerCase() : a[order]
-        const elB = typeof b[order] === 'string' ? b[order].toLowerCase() : b[order]
-        if (elA > elB) return 1
-        else if (elA < elB) return -1
-        else return 0
-    }
-
     // Order by
     useEffect(() => {
         if (filteredNotes.length > 0) {
-            const newFilteredNotes = filteredNotes.sort(sortFunction)
+            const newFilteredNotes = 
+                filteredNotes.sort((a, b) => sortFunction(a, b, order))
             setFilteredNotes([...newFilteredNotes])
         }
     }, [order, filteredNotes.length]) // eslint-disable-line
@@ -77,12 +79,17 @@ function Overview(props: Props) {
     useEffect(() => {
         if (props.search) {
             const searchRegExp = new RegExp(`^${props.search}[a-z0-9-_]?`, 'i')
-            setFilteredNotes(filteredNotes.filter(note => note.title.toLowerCase().match(searchRegExp)))
+            setFilteredNotes(
+                filteredNotes.filter(
+                    note => note.title.toLowerCase().match(searchRegExp)
+                )
+            )
         } else setFilteredNotes(allNotes)
     }, [props.search]) // eslint-disable-line
 
     function addNote() {
         const user = auth.currentUser?.uid
+        setLoading(true)
         database.collection('users').doc(user).get()
             .then(doc => {
                 const userNotes = doc.data()!.notes
@@ -100,6 +107,7 @@ function Overview(props: Props) {
                 database.collection('users').doc(user).update({
                     notes: userNotes
                 }).then(() => {
+                    setLoading(false)
                     props.setCurrentNote(userNote)
                     props.history.push('/home/edit-note')
                 })
@@ -149,13 +157,14 @@ function Overview(props: Props) {
         },
         {
             text: "Deleted notes",
-            icon: <Icon icon={trash_can_outline} />,
+            icon: <Icon icon={trashCanIcon} />,
             type: "normal",
             click: () => props.history.push("/home/deleted-notes")
         }
     ]
 
     useEffect(() => {
+        props.toggleMultiselection(false)
         auth.onAuthStateChanged(authState => {
             if (authState) {
                 const user = auth.currentUser?.uid
@@ -171,7 +180,7 @@ function Overview(props: Props) {
 
     useEffect(() => {
         setAllNotes(props.notes)
-        setFilteredNotes([...props.notes.sort(sortFunction)])
+        setFilteredNotes([...props.notes.sort((a, b) => sortFunction(a, b, order))])
     }, [props.notes]) // eslint-disable-line
 
     function onNoteCardClick(noteId: string) {
@@ -187,120 +196,104 @@ function Overview(props: Props) {
         }
     }
 
-    function selectAll() {
-        const newNotes = filteredNotes.map(note => ({ ...note, selected: true }))
-        setFilteredNotes(newNotes)
-    }
-
-    function unselectAll() {
-        const newNotes = filteredNotes.map(note => ({ ...note, selected: false }))
-        setFilteredNotes(newNotes)
-    }
-
-    function invertSelection() {
-        const newNotes = filteredNotes.map(note => {
-            if (note.selected) return { ...note, selected: false }
-            else return { ...note, selected: true }
-        })
-        setFilteredNotes(newNotes)
-    }
-
     function deleteSelected() {
         const delNotes = filteredNotes.filter(note => note.selected)
-        const fltdNotes = filteredNotes.filter(note => !note.selected)
-
-        const user = auth.currentUser?.uid
-        database.collection('users').doc(user).get()
-            .then(snapshot => {
-                const notes: Note[] = snapshot.data()!.notes
-                const trash: Note[] = snapshot!.data()!.trash
-
-                delNotes.forEach(delNote => {
-                    notes.forEach((note, index) => {
-                        if (delNote.id === note.id) {
-                            notes.splice(index, 1)
-                            Object.assign(delNote, { deleted: Date.now(), selected: false })
-                            trash.push(delNote)
-                        }
-                    })
-                })
-
-                database.collection('users').doc(user).update({ notes, trash })
-                    .then(() => {
-                        props.setNotes(notes)
-                        props.sendToTrash(trash)
-                    })
+        setLoading(true)
+        sendNotesTo("notes", "trash", delNotes)
+            .then(res => {
+                setLoading(false)
+                props.setNotes(res.notes)
+                props.setTrash(res.trash)
+                props.toggleMultiselection()
             })
-
-        delNotes.forEach(note => {
-            note.deleted = Date.now()
-            note.selected = false
-        })
-
-        props.sendToTrash(delNotes)
-        props.toggleMultiselection()
-
-        setFilteredNotes(fltdNotes)
     }
 
     return (
         <div className="Overview">
             {
                 loading
-                ? <div className="SpinnerResizer"><Spinner /></div>
-                : (
-                    <>
-                        <div className={["MultiselectionOptions", props.multiselection ? "Show" : ""].join(' ')}>
-                            <Button onclick={selectAll}>
-                                <Icon icon={selectAllIcon} />
-                                <span>Select All</span>
-                            </Button>
-                            <Button onclick={unselectAll}>
-                                <Icon icon={selectOffIcon} />
-                                <span>Unselect All</span>
-                            </Button>
-                            <Button onclick={invertSelection}>
-                                <Icon icon={selectInverseIcon} />
-                                <span>Invert Selection</span>
-                            </Button>
-                            <Button btnType="Danger" onclick={deleteSelected}>
-                                <Icon icon={deleteCircleOutlineIcon} />
-                                <span>Delete Selected</span>
-                            </Button>
-                        </div>
-
-                        <div className="NotesOverview">
-                            {filteredNotes.map(note => (
-                                <div key={note.id} className="NoteCard" onClick={() => onNoteCardClick(note.id)}>
-                                    <div className="NoteCardSelector" style={{ backgroundColor: note.color }}>
-                                        {
-                                            props.multiselection &&
-                                            <input
-                                                type="checkbox"
-                                                readOnly
-                                                checked={note.selected}
-                                            />
-                                        }
-                                    </div>
-                                    <div className="NoteCardContent">
-                                        <span>{note.title}</span>
-                                        <p>
-                                            {
-                                                note.content && (note.content.length <= 40
-                                                    ? note.content
-                                                    : note.content.substring(0, 39).concat('...'))
-                                            }
-                                        </p>
-                                    </div>
-                                </div>
-                            ))}
-                            <div className="AddNoteCard" onClick={addNote}>
-                                <span>Add Note</span>
-                                <Icon icon={plusIcon} />
+                    ? <div className="SpinnerResizer"><Spinner /></div>
+                    : (
+                        <>
+                            <div className={[
+                                "MultiselectionOptions",
+                                props.multiselection && filteredNotes.length > 0
+                                    ? "Show"
+                                    : ""
+                            ].join(' ')}>
+                                <Button
+                                    onclick={
+                                        () => setFilteredNotes(
+                                            selectAll(filteredNotes)
+                                        )}>
+                                    <Icon icon={selectAllIcon} />
+                                    <span>Select All</span>
+                                </Button>
+                                <Button
+                                    onclick={
+                                        () => setFilteredNotes(
+                                            unselectAll(filteredNotes)
+                                        )}>
+                                    <Icon icon={selectOffIcon} />
+                                    <span>Unselect All</span>
+                                </Button>
+                                <Button
+                                    onclick={() => setFilteredNotes(
+                                        invertSelection(filteredNotes)
+                                    )}>
+                                    <Icon icon={selectInverseIcon} />
+                                    <span>Invert Selection</span>
+                                </Button>
+                                <Button btnType="Danger" onclick={deleteSelected}>
+                                    <Icon icon={deleteCircleIcon} />
+                                    <span>Delete Selected</span>
+                                </Button>
                             </div>
-                        </div>
-                    </>
-                )
+
+                            <div className="NotesOverview">
+                                {filteredNotes.map(note => (
+                                    <div
+                                        key={note.id}
+                                        className="NoteCard"
+                                        onClick={() => onNoteCardClick(note.id)}
+                                    >
+                                        <div
+                                            className="NoteCardSelector"
+                                            style={{ backgroundColor: note.color }}
+                                        >
+                                            {
+                                                props.multiselection &&
+                                                <input
+                                                    type="checkbox"
+                                                    readOnly
+                                                    checked={note.selected}
+                                                />
+                                            }
+                                        </div>
+                                        <div className="NoteCardContent">
+                                            <span>{note.title}</span>
+                                            <p>
+                                                {
+                                                    note.content &&
+                                                    (
+                                                        note.content.length <= 40
+                                                            ? note.content
+                                                            : note.content
+                                                                .substring(0, 39)
+                                                                .concat('...')
+                                                    )
+                                                }
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))}
+                                <div className="AddNoteCard" onClick={addNote}>
+                                    <span>Add Note</span>
+                                    <Icon icon={plusIcon} />
+                                </div>
+                            </div>
+                        </>
+                    )
             }
         </div>
     )
@@ -318,8 +311,8 @@ function mapStateToProps(state: any) {
 function mapDispatchToProps(dispatch: any) {
     return {
         setOptions(options: any) { dispatch(navActions.setOptions(options)) },
-        toggleMultiselection() { dispatch(navActions.toggleMultiselection()) },
-        sendToTrash(trash: any) { dispatch(deletedNotesActions.setTrash(trash)) },
+        toggleMultiselection(mode?: boolean) { dispatch(navActions.toggleMultiselection(mode)) },
+        setTrash(trash: any) { dispatch(deletedNotesActions.setTrash(trash)) },
         editNote(note: Note) { dispatch(navActions.setCurrentNote(note)) },
         setNotes(notes: Note[]) { dispatch(overviewActions.setNotes(notes)) },
         setCurrentNote(note: Note) { dispatch(navActions.setCurrentNote(note)) }
