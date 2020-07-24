465748c46d7c29c0d11435201ca75f01855210dc
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2020-07-24 14:49:03 -0300

    Refactored to accept multiple action buttons

--- src/components/UI/FeedbackModal/index.tsx
+++ src/components/UI/FeedbackModal/index.tsx
@@ -10,22 +10,28 @@ interface Props {
     feedback: string,
     icon?: JSX.Element,
     hasAction: boolean,
-    actionLabel?: string,
-    onModalAction?: (...params: any[]) => void
+    actionLabel?: string[],
+    onModalAction?: ((...params: any[]) => void)[]
 }
 
 function FeedbackModal(props: Props) {
+    let actions: JSX.Element[] | null = null
+
+    if (props.hasAction) {
+        actions = props.onModalAction!.map((action, i) => (
+            <Button key={i} onclick={action}>
+                {props.actionLabel![i]}
+            </Button>
+        ))
+    }
+
     return (
         <div className="FeedbackModal">
             <span className="ModalContent">{props.feedback}</span>
             {props.icon && props.icon}
-            {
-                props.hasAction && (
-                    <Button onclick={props.onModalAction}>
-                        {props.actionLabel}
-                    </Button>
-                )
-            }
+            <div className="ModalActions">
+                {props.hasAction && actions}
+            </div>
         </div>
     )
 }
