63a9e3375fcd9b930afabaab35af9beb270f942f
Gabriel Melo Cavalcante <contato.gabrielmcavalcante@gmail.com>
2020-07-30 10:42:41 -0300

    Update init.ts

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -3,15 +3,7 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBTvczkPKF5Ftp23C9sDKYrLx7U3KMlWcY",
-    authDomain: "notes-5cf6a.firebaseapp.com",
-    databaseURL: "https://notes-5cf6a.firebaseio.com",
-    projectId: "notes-5cf6a",
-    storageBucket: "notes-5cf6a.appspot.com",
-    messagingSenderId: "1014809961003",
-    appId: "1:1014809961003:web:8bce82a789a68337d6006e"
-}
+const firebaseConfig = {/* YOUR FIREBASE PROJECT CONFIG*/}
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 const database = firebaseApp.firestore()
